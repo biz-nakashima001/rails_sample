561e1d8081f7ca5fa375070fc8a32aaf1d80f9c7
中嶋　慎也 <nakasin52@gmail.com>
2018-11-07 20:41:55 +0900

    delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,12 @@ class PostsController < ApplicationController
           render 'edit'
       end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
   
 private
   def post_params
